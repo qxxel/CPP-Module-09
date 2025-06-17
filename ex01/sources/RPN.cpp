4d457a6def0c0d869c88459080b0cb4f632477ea
agerbaud <agerbaud@student.42lyon.fr>
2025-06-17 16:55:56 +0200

    fix ex01 by checking if numbers are less than 10

--- ex01/sources/RPN.cpp
+++ ex01/sources/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: agerbaud <agerbaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 17:37:28 by agerbaud          #+#    #+#             */
-/*   Updated: 2025/04/08 18:33:40 by agerbaud         ###   ########.fr       */
+/*   Updated: 2025/06/17 16:54:46 by agerbaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,42 +19,58 @@ int	calculate(std::string input)
 	for (size_t i = 0; i < input.size(); i++)
 	{
 		char	c = input[i];
+
 		if (c == ' ') { }
+
 		else if (isdigit(c))
 		{
+			if (isdigit(input[i + 1]))
+			{
+				std::cerr << RED << "Error: numbers must be less than 10" << RESET << std::endl;
+				return (1);
+			}
+
 			int	number = c - '0';
 			stack.push(number);
 		}
+
 		else if (c == '+' && stack.size() >= 2)
 		{
 			double	a = stack.top();
 			stack.pop();
 			double	b = stack.top();
 			stack.pop();
+
 			stack.push(b + a);
 		}
+
 		else if (c == '-' && stack.size() >= 2)
 		{
 			double	a = stack.top();
 			stack.pop();
 			double	b = stack.top();
 			stack.pop();
+
 			stack.push(b - a);
 		}
+
 		else if (c == '*' && stack.size() >= 2)
 		{
 			double	a = stack.top();
 			stack.pop();
 			double	b = stack.top();
 			stack.pop();
+
 			stack.push(b * a);
 		}
+
 		else if (c == '/' && stack.size() >= 2)
 		{
 			double	a = stack.top();
 			stack.pop();
 			double	b = stack.top();
 			stack.pop();
+
 			if (a == 0)
 			{
 				std::cerr << RED << "Error: division by 0" << RESET << std::endl;
@@ -63,17 +79,20 @@ int	calculate(std::string input)
 			else
 				stack.push(b / a);
 		}
+
 		else
 		{
 			std::cerr << RED << "Error: bad input" << RESET << std::endl;
 			return (1);
 		}
 	}
+
 	if (stack.size() != 1)
 	{
 		std::cerr << RED << "Error: bad input" << RESET << std::endl;
 		return (1);
 	}
+
 	std::cout << "Result: " << stack.top() << std::endl;
 	return (0);
 }
